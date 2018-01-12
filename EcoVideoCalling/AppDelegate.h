19843f9a98fb0b386140aead2367bb4a1f6d57e0
ihub <ihub@ihubs-mac-mini-3.local>
2018-01-12 11:29:28 +0530

    Initial Commit

---
+++ EcoVideoCalling/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EcoVideoCalling
+//
+//  Created by ihub on 12/01/18.
+//  Copyright © 2018 ecoihub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
